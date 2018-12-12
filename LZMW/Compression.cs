fb4413cd3fb71a64c8241b6e032d8cd551cab9fc
mikuuu21 <lukania94@gmail.com>
2018-12-12 01:51:49 +0100

    LZMWCompressionFIrst

--- LZMW/Compression.cs
+++ LZMW/Compression.cs
@@ -28,29 +28,65 @@ namespace LZMW
         {
 
 
+            string SpS = null;
+            string S = null;
+            string Sp = null;
 
-            string s = "";
-
+            
             List<int> output = new List<int>();
 
             for (int i = 0; i < input.Length; i++)
             {
 
-                int c = input[i];
-                string sc = s + c;
-
-                if (dict.ContainsKey(sc))
+                string c = input[i].ToString();
+                var matches = dict.Count(x => x.Key.Contains(c));
+                if(matches > 1)
                 {
-                    s = sc;
-                    
+                    string cs = null;
+                    for (int j = i+1 ; j < matches ; j++)
+                    {
+                        cs = c + input[j];
+                        var match = dict.Count(x => x.Key.Contains(cs));
+                        if (match == 1)
+                        {
+                            S = cs;
+                            output.Add(dict[S]);
+
+                        }
+                        else if (match > 1)
+                        {
+                            c = cs;
+                            continue;
+                        }
+                        else if (match == 0)
+                        {
+                            S = c;
+                            output.Add(dict[S]);
+                        }
+                        
+                    }
+
                 }
                 else
                 {
 
+                    S = c;
+                    output.Add(dict[S]);
+                }
 
-                    output.Add(dict[s]);
-                    dict.Add(sc, dict.Count());
-                    s = input[i].ToString();
+                SpS = S + Sp;
+
+                if (dict.ContainsKey(SpS))
+                {
+
+                    dict.Add(SpS, dict.Count());
+                    Sp = S;
+
+                }
+                else
+                {
+
+                    Sp = S;
                 }
 
               
